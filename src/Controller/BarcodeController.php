8eb17f2f719756d1c4b98e0561bea1f3d1a57290
Keller Alexandre <keller.alexandre0@gmail.com>
2023-10-12 17:30:06 +0200

    test is complete: a file is uploaded by ajax returned in base64 and result of barcode parsing is returned too

--- src/Controller/BarcodeController.php
+++ src/Controller/BarcodeController.php
@@ -2,12 +2,52 @@
 
 namespace Controller;
 
-use MerryGoblin/
+use MerryGoblin\BarcodeReader\Services\Barcode\BarcodeReader;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class BarcodeController
 {
-	public function parseAction()
+	public function uploadBarcodeAction()
 	{
+		$imageContent = '';
+		$tmpFilePath = '';
+		if (isset($_FILES['uploaded-photo'])) {
 
+			//	Uploaded file
+			$photoUploadFile = $_FILES['uploaded-photo'];
+			$tmpFilePath = $photoUploadFile['tmp_name'];
+			$fileType = $photoUploadFile['type'];
+			$fileOriginalName = $photoUploadFile['name'];
+
+			//	Image orientation
+			$exifResponse = @exif_read_data($tmpFilePath);
+			$orientation = ($exifResponse !== false && isset($exifResponse['Orientation'])) ? intval($exifResponse['Orientation']) : 1;
+
+			//	Image content
+			$imageContent = @file_get_contents($tmpFilePath);
+			if ($imageContent !== false) {
+				try {
+					//	Barcode parsing
+					$barcodeReader = new BarcodeReader();
+					$parsingResult = $barcodeReader->parse($tmpFilePath);
+				} catch(ProcessFailedException $e) {
+					$parsingResult = 'failed';
+				}
+			}
+		}
+
+		$data = [
+			'message' => 'The request was successful',
+			'file' => print_r($_FILES, true),
+			'filePath' => $tmpFilePath,
+			'imageContent' => base64_encode(print_r($imageContent, true)),
+			'parsingResult' => print_r($parsingResult, true),
+		];
+
+		http_response_code(200);
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($data);
+		exit();
 	}
 }
--- web/barcode-reader-api.php
+++ web/barcode-reader-api.php
@@ -2,4 +2,5 @@
 
 require_once(__DIR__."/../vendor/autoload.php");
 
-
+$controller = new Controller\BarcodeController();
+$controller->uploadBarcodeAction();
